12ced021ddf1c23958498316a51c2ca9f99a70f0
xHomu <nobisan@gmail.com>
2025-01-09 13:51:01 -0800

    layout

---
+++ app/layouts/sidebar.tsx
@@ -0,0 +1,62 @@
+import { Form, Link, Outlet } from "react-router";
+import type { Route } from "./+types/sidebar";
+
+import { getContacts } from "../data";
+
+export async function clientLoader() {
+  const contacts = await getContacts();
+
+  return { contacts };
+}
+
+export default function App({ loaderData }: Route.ComponentProps) {
+  const { contacts } = loaderData;
+
+  return (
+    <>
+      <div id="sidebar">
+        <h1>
+          <Link to="about">About</Link>
+        </h1>
+        <h1>React Router Contacts</h1>
+        <div>
+          <Form id="search-form" role="search">
+            <input
+              aria-label="Search contacts"
+              id="q"
+              name="q"
+              placeholder="Search"
+              type="search"
+            />
+            <div aria-hidden hidden={true} id="search-spinner" />
+          </Form>
+          <Form method="post">
+            <button type="submit">New</button>
+          </Form>
+        </div>
+        <nav>
+          <ul>
+            {contacts.length ? (
+              contacts.map((contact) => (
+                <li key={contact.id}>
+                  <Link to={`contacts/${contact.id}`}>
+                    {contact.first || contact.last
+                      ? `${contact.first} ${contact.last}`
+                      : null}
+                  </Link>
+                </li>
+              ))
+            ) : (
+              <p>
+                <i>No contacts</i>
+              </p>
+            )}
+          </ul>
+        </nav>
+      </div>
+      <div id="detail">
+        <Outlet />
+      </div>
+    </>
+  );
+}
--- app/root.tsx
+++ app/root.tsx
@@ -11,60 +11,67 @@ import type { Route } from "./+types/root";
 import appStylesHref from "./app.css?url";
 import { getContacts } from "./data";
 
-export async function clientLoader() {
-  const contacts = await getContacts();
-
-  return { contacts };
+export default function App() {
+  return <Outlet />;
 }
 
-export default function App({ loaderData }: Route.ComponentProps) {
-  const { contacts } = loaderData;
+// export async function clientLoader() {
+//   const contacts = await getContacts();
 
-  return (
-    <>
-      <div id="sidebar">
-        <h1>React Router Contacts</h1>
-        <div>
-          <Form id="search-form" role="search">
-            <input
-              aria-label="Search contacts"
-              id="q"
-              name="q"
-              placeholder="Search"
-              type="search"
-            />
-            <div aria-hidden hidden={true} id="search-spinner" />
-          </Form>
-          <Form method="post">
-            <button type="submit">New</button>
-          </Form>
-        </div>
-        <nav>
-          <ul>
-            {contacts.length ? (
-              contacts.map((contact) => (
-                <li key={contact.id}>
-                  <Link to={`contacts/${contact.id}`}>
-                    {contact.first || contact.last
-                      ? `${contact.first} ${contact.last}`
-                      : null}
-                  </Link>
-                </li>
-              ))
-            ) : (
-              <p>
-                <i>No contacts</i>
-              </p>
-            )}
-          </ul>
-        </nav>
-      </div>
-      <div id="detail">
-        <Outlet />
-      </div>
-    </>
-  );
-}
+//   return { contacts };
+// }
+
+// export default function App({ loaderData }: Route.ComponentProps) {
+//   const { contacts } = loaderData;
+
+//   return (
+//     <>
+//       <div id="sidebar">
+//         <h1>
+//           <Link to="about">About</Link>
+//         </h1>
+//         <h1>React Router Contacts</h1>
+//         <div>
+//           <Form id="search-form" role="search">
+//             <input
+//               aria-label="Search contacts"
+//               id="q"
+//               name="q"
+//               placeholder="Search"
+//               type="search"
+//             />
+//             <div aria-hidden hidden={true} id="search-spinner" />
+//           </Form>
+//           <Form method="post">
+//             <button type="submit">New</button>
+//           </Form>
+//         </div>
+//         <nav>
+//           <ul>
+//             {contacts.length ? (
+//               contacts.map((contact) => (
+//                 <li key={contact.id}>
+//                   <Link to={`contacts/${contact.id}`}>
+//                     {contact.first || contact.last
+//                       ? `${contact.first} ${contact.last}`
+//                       : null}
+//                   </Link>
+//                 </li>
+//               ))
+//             ) : (
+//               <p>
+//                 <i>No contacts</i>
+//               </p>
+//             )}
+//           </ul>
+//         </nav>
+//       </div>
+//       <div id="detail">
+//         <Outlet />
+//       </div>
+//     </>
+//   );
+// }
 
 // The Layout component is a special export for the root route.
 // It acts as your document's "app shell" for all route components, HydrateFallback, and ErrorBoundary
